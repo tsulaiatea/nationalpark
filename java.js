7f4aab4aa476344be490b15867a650b98a0e065a
tsulaiatea <teatsulaiaa@gmail.com>
2025-03-02 19:24:30 +0400

    add news page

--- java.js
+++ java.js
@@ -3,11 +3,15 @@ let burger = document.getElementById ("burger");
 
 burger. addEventListener("click", function() {
     if(links.classList.contains("active")){
+        // ნავიგაციას აქრობს
         links.classList.remove("active")
+        // ბურგერ მენიუდ გადააქცევს  X აიქონს
         burger.innerHTML =  '<i class="fa-solid fa-bars"></i>'
 
     }else {
+        // ეს გამოიტანს ნავიგაციის მენიუს
         links.classList. add("active")
+        // ეს გადააქცევს X -ად
         burger.innerHTML = '<i class="fa-solid fa-circle-xmark"></i>'
     }
     
