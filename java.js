8e9531aa0fadea26bbee31f66fefb62838bf11d7
tsulaiatea <teatsulaiaa@gmail.com>
2025-03-06 23:31:22 +0400

    add slider

--- java.js
+++ java.js
@@ -1,3 +1,4 @@
+// "use strict"
 let links = document.getElementById ("links");
 let burger = document.getElementById ("burger");
 
@@ -16,3 +17,82 @@ burger. addEventListener("click", function() {
     }
     
 });
+
+// slider
+let data = [
+    {
+       id: 1,
+       title:"pirveli",
+       imageUrl:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fnationalparks.ge%2Fka%2Fsite%2Fkolxetinp&psig=AOvVaw3dvZaN5mGOpI1lABHYULYE&ust=1741372315680000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCMiynsWL9osDFQAAAAAdAAAAABAE"
+    },
+    {
+        id: 2,
+        title:"meore",
+        imageUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fgeorgia.travel%2Fka%2F%25E1%2583%2599%25E1%2583%259D%25E1%2583%259A%25E1%2583%25AE%25E1%2583%2594%25E1%2583%2597%25E1%2583%2598%25E1%2583%25A1-%25E1%2583%2594%25E1%2583%25A0%25E1%2583%259D%25E1%2583%2595%25E1%2583%259C%25E1%2583%25A3%25E1%2583%259A%25E1%2583%2598-%25E1%2583%259E%25E1%2583%2590%25E1%2583%25A0%25E1%2583%2599%25E1%2583%2598&psig=AOvVaw3dvZaN5mGOpI1lABHYULYE&ust=1741372315680000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCMiynsWL9osDFQAAAAAdAAAAABAP"
+    },
+    {
+        id: 3,
+        title:"mesame",
+        imageUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fnationalparks.ge%2Fka%2Fsite%2Fkolxetinp&psig=AOvVaw3MmO6RUQ28qfuiyfo5bKTm&ust=1741372562744000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCOi_pMKM9osDFQAAAAAdAAAAABAE"  
+    },
+
+];
+
+let sliderContainer = document.getElementById("slider")
+let arrowLeft = document.getElementById("arrow-left")
+let arrowRight = document.getElementById("arrow-right")
+
+let sliderIndex = 0
+
+// სურათის შექმნის ფუნქცია
+
+function createImg(item){
+    sliderContainer.style.backgroundImage = 'url(${item.imageUrl})'
+    sliderContainer.style.backgroundRepeat = 'no-repeat'
+    sliderContainer.style.backgroundSize = 'cover'
+} 
+
+// სათაურის გამოტანის ფუნქცია
+
+function createH2tag(item) {
+    
+
+let h2tag = document.createElement('h2')
+h2tag.textContent = item.title
+h2tag.classList.add('slider-title')
+return h2tag
+}  
+
+// მთავარი ფუნქცია, რომელშიც გამოჩნდება სურათები
+
+function setSlider(){
+    createImg(data[sliderIndex])
+    let title = createH2tag(data[sliderIndex])
+
+    sliderContainer.appendChild(title)
+    
+}
+
+function arrowLeft(){
+        if(sliderIndex === 0){
+            sliderIndex = data.length-1
+        }else {
+            sliderIndex --
+        }
+        setSlider()
+    }
+
+    function arrowLRight(){
+        if(sliderIndex === data.length-1){
+            sliderIndex = 0
+        }else {
+            sliderIndex ++
+        }
+        setSlider()
+    }
+    arrowLeft.addEventListener('click', arrowLeft)
+
+
+
+
+setSlider()
